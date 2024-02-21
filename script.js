f441d4e55525a8a2587f3fcae3433d6615c7e082
Amit Rathore <amitrathore04984@gmail.com>
2024-02-21 17:07:47 +0530

    second

--- script.js
+++ script.js
@@ -78,3 +78,78 @@ function page1Animation(){
 }
 
 page1Animation()
+
+// var prj = document.querySelector(".project");
+
+// prj.addEventListener("mousemove", function() {
+//     gsap.to(".project img", {
+//         scale: 1.04
+//     });
+// });
+// prj.addEventListener("mouseleave", function() {
+//     gsap.to(".project img", {
+//         scale: 1
+//     });
+// });
+
+// var projects = document.querySelectorAll(".project");
+
+// projects.forEach(function (project) {
+//     project.addEventListener("mousemove", function () {
+//         gsap.to(this.querySelector("img"), {
+//             scale: 1.03
+//         });
+//     });
+
+//     project.addEventListener("mouseleave", function () {
+//         gsap.to(this.querySelector("img"), {
+//             scale: 1
+//         });
+//     });
+// });
+
+// var projects = document.querySelectorAll(".project");
+
+// projects.forEach(function (project) {
+//     project.addEventListener("mouseenter", function () {
+//         gsap.to(this.querySelector("img"), {
+//             scale: 1.04
+//         });
+
+//         gsap.to("body", {
+//                 backgroundColor: "yellow" // Change the background color on mouseenter
+//         });
+//     });
+
+//     project.addEventListener("mouseleave", function () {
+//         gsap.to(this.querySelector("img"), {
+//             scale: 1
+//         });
+
+//         gsap.to("body", {
+//             backgroundColor: "black" // Revert to normal background color on mouseleave
+//         });
+//     });
+// });
+
+
+var projects = document.querySelectorAll(".project");
+var body = document.querySelector("html,body");
+
+projects.forEach(function (project) {
+    project.addEventListener("mouseenter", function () {
+        gsap.to(this.querySelector("img"), {
+            scale: 1.04
+        });
+
+        body.style.backgroundColor = "yellow"; // Change the background color on mouseenter
+    });
+
+    project.addEventListener("mouseleave", function () {
+        gsap.to(this.querySelector("img"), {
+            scale: 1
+        });
+
+        body.style.backgroundColor = ""; // Revert to normal background color on mouseleave
+    });
+});
