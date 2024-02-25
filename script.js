7433e91ce79a8a03bd3ffceff585dae917e2acd8
Amit Rathore <amitrathore04984@gmail.com>
2024-02-26 00:04:19 +0530

    structure is completed

--- script.js
+++ script.js
@@ -1,39 +1,79 @@
-function page1Animation(){
-    window.addEventListener("mousemove", function(dets){
-        gsap.to(".cursor",{
-            x:dets.x,
-            y:dets.y
+function locomotive(){
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+    
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+    
+    
+    
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+    
+    
+    
+    
+}
+
+locomotive()
+
+
+function page1Animation() {
+    window.addEventListener("mousemove", function (dets) {
+        gsap.to(".cursor", {
+            x: dets.x,
+            y: dets.y
         })
     })
-    document.querySelector("#over").addEventListener("mousemove", function(){
-        gsap.to(".cursor",{
-            scale:5
+    document.querySelector("#over").addEventListener("mousemove", function () {
+        gsap.to(".cursor", {
+            scale: 5
         })
     })
-    document.querySelector("#over").addEventListener("mouseleave", function(){
-        gsap.to(".cursor",{
-            scale:1
+    document.querySelector("#over").addEventListener("mouseleave", function () {
+        gsap.to(".cursor", {
+            scale: 1
         })
     })
-    document.querySelector("#title-section h1").addEventListener("mousemove", function(){
-        gsap.to(".cursor",{
-            scale:5
+    document.querySelector("#title-section h1").addEventListener("mousemove", function () {
+        gsap.to(".cursor", {
+            scale: 5
         })
     })
-    document.querySelector("#title-section h1").addEventListener("mouseleave", function(){
-        gsap.to(".cursor",{
-            scale:1
+    document.querySelector("#title-section h1").addEventListener("mouseleave", function () {
+        gsap.to(".cursor", {
+            scale: 1
         })
     })
     var tl = gsap.timeline();
-    
+
     var impo = document.querySelectorAll(".impo");
-    
+
     impo.forEach(function (e) {
         e.addEventListener("mouseenter", function () {
             // Reset the timeline at the beginning of each mouseenter
             tl.clear();
-    
+
             tl.to(".first", {
                 y: -200,
                 // opacity: 0
@@ -43,19 +83,19 @@ function page1Animation(){
                 // opacity: 0
             }, "a");
             tl.to("#over img", {
-              scale:1.04
+                scale: 1.04
                 // opacity: 0
             }, "a");
             tl.to("#title-section h1", {
-                scale:1.08
-                  // opacity: 0
-              }, "a");
+                scale: 1.08
+                // opacity: 0
+            }, "a");
         });
-    
+
         e.addEventListener("mouseleave", function () {
             // Reset the timeline at the beginning of each mouseleave
             tl.clear();
-    
+
             tl.to(".first", {
                 y: -40,
                 // transform: "translateY(-25%)"
@@ -65,91 +105,93 @@ function page1Animation(){
                 // opacity: 0
             }, "a");
             tl.to("#over img", {
-                scale:1
-                  // opacity: 0
-              }, "a");
-              tl.to("#title-section h1", {
-                scale:1
-                  // opacity: 0
-              }, "a");
+                scale: 1
+                // opacity: 0
+            }, "a");
+            tl.to("#title-section h1", {
+                scale: 1
+                // opacity: 0
+            }, "a");
         });
     });
-    
-}
 
+}
 page1Animation()
 
-// var prj = document.querySelector(".project");
 
-// prj.addEventListener("mousemove", function() {
-//     gsap.to(".project img", {
-//         scale: 1.04
-//     });
-// });
-// prj.addEventListener("mouseleave", function() {
-//     gsap.to(".project img", {
-//         scale: 1
-//     });
-// });
-
-// var projects = document.querySelectorAll(".project");
-
-// projects.forEach(function (project) {
-//     project.addEventListener("mousemove", function () {
-//         gsap.to(this.querySelector("img"), {
-//             scale: 1.03
-//         });
-//     });
-
-//     project.addEventListener("mouseleave", function () {
-//         gsap.to(this.querySelector("img"), {
-//             scale: 1
-//         });
-//     });
-// });
-
-// var projects = document.querySelectorAll(".project");
-
-// projects.forEach(function (project) {
-//     project.addEventListener("mouseenter", function () {
-//         gsap.to(this.querySelector("img"), {
-//             scale: 1.04
-//         });
-
-//         gsap.to("body", {
-//                 backgroundColor: "yellow" // Change the background color on mouseenter
-//         });
-//     });
-
-//     project.addEventListener("mouseleave", function () {
-//         gsap.to(this.querySelector("img"), {
-//             scale: 1
-//         });
-
-//         gsap.to("body", {
-//             backgroundColor: "black" // Revert to normal background color on mouseleave
-//         });
-//     });
-// });
-
-
-var projects = document.querySelectorAll(".project");
-var body = document.querySelector("html,body");
-
-projects.forEach(function (project) {
-    project.addEventListener("mouseenter", function () {
-        gsap.to(this.querySelector("img"), {
-            scale: 1.04
-        });
-
-        body.style.backgroundColor = "yellow"; // Change the background color on mouseenter
-    });
-
-    project.addEventListener("mouseleave", function () {
-        gsap.to(this.querySelector("img"), {
-            scale: 1
-        });
-
-        body.style.backgroundColor = ""; // Revert to normal background color on mouseleave
-    });
-});
+    //   gsap.from(".project #image1",{
+    //     height:"0%",
+    //     opacity:0,
+        
+    // })
+    var ti = gsap.timeline();
+    ti.from("#page1 .impo",{
+        scale:0,
+        opacity:0,
+        stagger:1
+    })
+    ti.to(".project #image1",{
+        opacity:1,
+        height:"100%",
+        scrollTrigger :{
+            trigger: " #p1",
+            scroller: "#main",
+            // markers: true,
+            start: "top 40%",
+            end : "top 40%",
+            scrub:1,
+            pin:true,
+        },
+    }) 
+    ti.to(".project #image2",{
+        opacity:1,
+        height:"100%",
+        scrollTrigger :{
+            trigger: " #p2",
+            scroller: "#main",
+            // markers: true,
+            start: "top 40%",
+            end : "top 40%",
+            scrub:1,
+            pin:true,
+        },
+    }) 
+       ti.to(".project #image3",{
+        opacity:1,
+        height:"100%",
+        scrollTrigger :{
+            trigger: " #p3",
+            scroller: "#main",
+            // markers: true,
+            start: "top 40%",
+            end : "top 40%",
+            scrub:1,
+            pin:true,
+        },
+    })
+    ti.to(".project #image4",{
+        opacity:1,
+        height:"100%",
+        scrollTrigger :{
+            trigger: " #p4",
+            scroller: "#main",
+            // markers: true,
+            start: "top 40%",
+            end : "top 40%",
+            scrub:1,
+            pin:true,
+        },
+    })
+    ti.to(".project #image5",{
+        opacity:1,
+        height:"100%",
+        scrollTrigger :{
+            trigger: " #p5",
+            scroller: "#main",
+            // markers: true,
+            start: "top 40%",
+            end : "top 40%",
+            scrub:1,
+            pin:true,
+        },
+    })
